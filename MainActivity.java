d8c3b8a196cdf39c2c6eaf430fa94c7ccd21a09b
ananya23456 <ananyank2003@gmail.com>
2026-09-01 16:01:59 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.program4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText name, password;
+    RadioGroup gender;
+    CheckBox terms;
+    Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.name);
+        password = findViewById(R.id.password);
+        gender = findViewById(R.id.gender);
+        terms = findViewById(R.id.terms);
+        submit = findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (name.getText().toString().isEmpty()) {
+                    name.setError("Enter your name");
+                    return;
+                }
+                if (password.getText().toString().isEmpty()) {
+                    password.setError("Enter your password");
+                    return;
+                }
+                if (password.length() < 8) {
+                    password.setError("Password must contain 8 characters");
+                    return;
+                }
+                if (gender.getCheckedRadioButtonId() == -1) {
+                    Toast.makeText(MainActivity.this,
+                            "Select gender",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (!terms.isChecked()) {
+                    Toast.makeText(MainActivity.this,
+                            "Accept terms and conditions",
+                            Toast.LENGTH_SHORT).show();
+                }
+                Toast.makeText(MainActivity.this, "Registration Sucessful",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+    }
+}
